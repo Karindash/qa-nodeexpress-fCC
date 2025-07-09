5a8a34ad68ee69e8f267fab26f48abebfce8026f
Karindash <karindashr@gmail.com>
2025-07-09 19:13:48 +0000

    feat: updating serialization to be implement on database

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
-const objectID = require('mongodb');
+const ObjectID = require('mongodb');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
@@ -33,18 +33,28 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
-  // Change the response to render the Pug template
-  res.render('index', { title: 'Hello', message: 'Please log in' });
-});
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-})
+  app.route('/').get((req, res) => {
+    // Change the response to render the Pug template
+    res.render('index', { 
+      title: 'Connected to Database', 
+      message: 'Please log in' });
+  });
 
-passport.deserializeUser((id, done) => {
-  myDB.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, null);
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  })
+
+  passport.deserializeUser((id, done) => {
+    myDB.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    })
+  })  
+}).catch(err => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: err, message: 'Unable to connect to database' });
   })
 })
 
